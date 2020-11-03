216bf5ae7418798f65ffdea579321bfe982471f5
WilsonStiven <wquintero@josegalan.edu.co>
2020-11-03 16:06:32 -0500

    POO

---
+++ Programacion_estructura.js
@@ -0,0 +1,19 @@
+//definir variables
+
+let nombre = 'Derly';
+let apellido = 'Murcia';
+let edad = 23;
+
+console.log(`hola mi nombre es: ${nombre} ${apellido} y tengo ${edad} años`); 
+
+// función
+
+let num = 20;
+
+function multiplicar(valor) {
+    return valor*valor;
+}
+
+let resultado= multiplicar(5)*num;
+console.log(resultado)
+
